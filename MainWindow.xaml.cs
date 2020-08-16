d693dbe6ba6087523cb02b46df254e81c0dba280
Cheetack <ken.tsuchiya.7q@stu.hosei.ac.jp>
2020-08-16 20:50:48 +0900

    add pad input thread and func.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -13,7 +13,9 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.IO;
-using Microsoft.Win32;  // ファイル選択ダイアログの名前空間を using
+using System.Threading;
+using Microsoft.Win32;          // ファイル選択ダイアログの名前空間を using
+using SharpDX.DirectInput;      // ゲームパッド入力
 
 namespace OXIDEfactory
 {
@@ -22,6 +24,9 @@ namespace OXIDEfactory
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool pad_update_thread_flag = true;
+        private Joystick pad_handle;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -53,27 +58,170 @@ namespace OXIDEfactory
             }
         }
 
+        // Joystick入力関連
+        public void PadInit()
+        {
+            // 入力周りの初期化
+            DirectInput dinput = new DirectInput();
+            if (dinput != null)
+            {
+                // 使用するゲームパッドのID
+                var joystickGuid = Guid.Empty;
+                // ゲームパッドからゲームパッドを取得する
+                if (joystickGuid == Guid.Empty)
+                {
+                    foreach (DeviceInstance device in dinput.GetDevices(DeviceType.Gamepad, DeviceEnumerationFlags.AllDevices))
+                    {
+                        joystickGuid = device.InstanceGuid;
+                        break;
+                    }
+                }
+                // ジョイスティックからゲームパッドを取得する
+                if (joystickGuid == Guid.Empty)
+                {
+                    foreach (DeviceInstance device in dinput.GetDevices(DeviceType.Joystick, DeviceEnumerationFlags.AllDevices))
+                    {
+                        joystickGuid = device.InstanceGuid;
+                        break;
+                    }
+                }
+                // 見つかった場合
+                if (joystickGuid != Guid.Empty)
+                {
+                    // パッド入力周りの初期化
+                    pad_handle = new Joystick(dinput, joystickGuid);
+                    if (pad_handle != null)
+                    {
+                        // バッファサイズを指定
+                        pad_handle.Properties.BufferSize = 128;
+
+                        // 相対軸・絶対軸の最小値と最大値を
+                        // 指定した値の範囲に設定する
+                        foreach (DeviceObjectInstance deviceObject in pad_handle.GetObjects())
+                        {
+                            switch (deviceObject.ObjectId.Flags)
+                            {
+                                case DeviceObjectTypeFlags.Axis:
+                                // 絶対軸or相対軸
+                                case DeviceObjectTypeFlags.AbsoluteAxis:
+                                // 絶対軸
+                                case DeviceObjectTypeFlags.RelativeAxis:
+                                    // 相対軸
+                                    var ir = pad_handle.GetObjectPropertiesById(deviceObject.ObjectId);
+                                    if (ir != null)
+                                    {
+                                        try
+                                        {
+                                            ir.Range = new InputRange(-1000, 1000);
+                                        }
+                                        catch (Exception) { }
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        public void UpdateForPad()
+        {
+            // フォームにフォーカスが無い場合、処理終了
+            //if (!Focused) { return; }
+            // 初期化が出来ていない場合、処理終了
+            if (pad_handle == null) { return; }
+
+            // キャプチャするデバイスを取得
+            pad_handle.Acquire();
+            pad_handle.Poll();
+
+            // ゲームパッドのデータ取得
+            var jState = pad_handle.GetCurrentState();
+            // 取得できない場合、処理終了
+            if (jState == null) { return; }
+
+            // 以下の処理は挙動確認用
+
+            // 挙動確認用：押されたキーをタイトルバーに表示する
+            // アナログスティックの左右軸
+            bool inputX = true;
+            if (jState.X > 300)
+            {
+                Console.WriteLine("入力キー：→");
+            }
+            else if (jState.X < -300)
+            {
+                Console.WriteLine("入力キー：←");
+            }
+            else
+            {
+                inputX = false;
+            }
+            // アナログスティックの上下軸
+            bool inputY = true;
+            if (jState.Y > 300)
+            {
+                Console.WriteLine("入力キー：↓");
+            }
+            else if (jState.Y < -300)
+            {
+                Console.WriteLine("入力キー：↑");
+            }
+            else
+            {
+                inputY = false;
+            }
+            // 未入力だった場合
+            if (!inputX && !inputY)
+            {
+                //Text = "入力キー：";
+            }
+
+            // ボタン判定
+            if (jState.Buttons[0])
+            {
+                // 1番ボタンが押されたらフォームが閉じるように処理
+                //Close();
+                Console.WriteLine("Buttons[0]");
+            }
+        }
+
+        // 別スレッドで実行するメソッド
+        private void PadThreadFunc()
+        {
+            while (pad_update_thread_flag)
+            {
+                UpdateForPad();
+
+                // CPUがフル稼働しないようにFPSの制限をかける
+                // ※簡易的に、おおよそ秒間30フレーム程度に制限
+                Thread.Sleep(32);
+            }
+
+            Console.WriteLine("JoyStickThread End.");
+        }
+
         // キー入力関連
         private void Window_KeyDown(object sender, KeyEventArgs e)
         {
             // 動画の選択
-            if (e.Key == Key.Up)
+            if (e.Key == System.Windows.Input.Key.Up)
             {
                 Console.WriteLine("up key");
             }
-            else if (e.Key == Key.Down)
+            else if (e.Key == System.Windows.Input.Key.Down)
             {
                 Console.WriteLine("down key");
             }
 
             // 動画の削除
-            if (e.Key == Key.Delete || e.Key == Key.Back)
+            if (e.Key == System.Windows.Input.Key.Delete || e.Key == System.Windows.Input.Key.Back)
             {
                 Console.WriteLine("delete or backspace key");
             }
 
             // 動画の再生と一時停止
-            if (e.Key == Key.Enter)
+            if (e.Key == System.Windows.Input.Key.Enter)
             {
                 Console.WriteLine("enter key");
             }
@@ -81,6 +229,19 @@ namespace OXIDEfactory
             Console.WriteLine(e.Key);
         }
 
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            PadInit();
 
+            // PadThreadFuncメソッドを別のスレッドで実行するThreadオブジェクトを作成する
+            Thread t = new Thread(new ThreadStart(PadThreadFunc));
+            t.Start();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // PadThreadFuncメソッドを終了
+            pad_update_thread_flag = false;
+        }
     }
 }
