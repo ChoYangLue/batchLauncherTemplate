b44df2c1d6482cb8781c8335bc0d2970d086f5fa
Cheetack <ken.tsuchiya.7q@stu.hosei.ac.jp>
2020-08-16 20:22:47 +0900

    import video file.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO;
+using Microsoft.Win32;  // ファイル選択ダイアログの名前空間を using
 
 namespace OXIDEfactory
 {
@@ -25,9 +27,60 @@ namespace OXIDEfactory
             InitializeComponent();
         }
 
+        public int loadVideo(string video_path)
+        {
+            if (File.Exists(video_path) != true)
+            {
+                Console.WriteLine(video_path+" does not exist.");
+                return -1;
+            }
+
+            MediaElementPlayView.Source = new Uri(video_path, UriKind.Relative);
+            MediaElementPlayView.LoadedBehavior = MediaState.Stop;
+
+            Console.WriteLine("loaded: " + video_path);
+            return 0;
+        }
+
         private void ImportButton_Click(object sender, RoutedEventArgs e)
         {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "映像ファイル (*.mp4)|*.mp4|全てのファイル (*.*)|*.*";
 
+            if (dialog.ShowDialog() == true)
+            {
+                loadVideo(dialog.FileName);
+            }
         }
+
+        // キー入力関連
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            // 動画の選択
+            if (e.Key == Key.Up)
+            {
+                Console.WriteLine("up key");
+            }
+            else if (e.Key == Key.Down)
+            {
+                Console.WriteLine("down key");
+            }
+
+            // 動画の削除
+            if (e.Key == Key.Delete || e.Key == Key.Back)
+            {
+                Console.WriteLine("delete or backspace key");
+            }
+
+            // 動画の再生と一時停止
+            if (e.Key == Key.Enter)
+            {
+                Console.WriteLine("enter key");
+            }
+
+            Console.WriteLine(e.Key);
+        }
+
+
     }
 }
