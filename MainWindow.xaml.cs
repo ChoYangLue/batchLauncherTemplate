19a75ee9f34e5383845d4590c0640cf77c9d4b28
Cheetack <vgntx91psandroid@gmail.com>
2022-07-31 21:42:23 +0900

    さよならのOXIDE。これからはテンプレートとして生きていく。

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,159 +13,36 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.IO;
-using System.Threading;
-using Microsoft.Win32;          // ファイル選択ダイアログの名前空間を using
-using SharpDX.DirectInput;      // ゲームパッド入力
 
-namespace OXIDEfactory
+namespace batchLauncherTemplate
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private UserInput user_inp;
-        private bool isPlaying = false;
-
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        public int loadVideo(string video_path)
+        private void FolderOpenButton_Click(object sender, RoutedEventArgs e)
         {
-            if (File.Exists(video_path) != true)
+            using (var cofd = new CommonOpenFileDialog()
             {
-                Console.WriteLine(video_path+" does not exist.");
-                return -1;
-            }
-
-            MediaElementPlayView.Source = new Uri(video_path, UriKind.Relative);
-            MediaElementPlayView.LoadedBehavior = MediaState.Manual;
-            MediaElementPlayView.Stop();
-
-            Console.WriteLine("loaded: " + video_path);
-            return 0;
-        }
-
-        public void pushSwitch(ref bool switch_button_flag)
-        {
-            if (switch_button_flag) { switch_button_flag = false; }
-            else { switch_button_flag = true; }
-        }
-
-        private void ImportButton_Click(object sender, RoutedEventArgs e)
-        {
-            var dialog = new OpenFileDialog();
-            dialog.Filter = "映像ファイル (*.mp4)|*.mp4|全てのファイル (*.*)|*.*";
-
-            if (dialog.ShowDialog() == true)
+                Title = "フォルダを選択してください",
+                RestoreDirectory = true, // 最後に選択したフォルダから始まる
+                IsFolderPicker = true, // フォルダ選択モードにする
+            })
             {
-                loadVideo(dialog.FileName);
-            }
-        }
-
-        // キー入力関連
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            // 動画の選択
-            if (e.Key == System.Windows.Input.Key.Up)
-            {
-                Console.WriteLine("up key");
-            }
-            else if (e.Key == System.Windows.Input.Key.Down)
-            {
-                Console.WriteLine("down key");
-            }
-
-            // 動画の削除
-            if (e.Key == System.Windows.Input.Key.Delete || e.Key == System.Windows.Input.Key.Back)
-            {
-                Console.WriteLine("delete or backspace key");
-            }
-
-            // 動画の再生と一時停止
-            if (e.Key == System.Windows.Input.Key.Enter)
-            {
-                Console.WriteLine("enter key");
-            }
-
-            if (e.Key == System.Windows.Input.Key.A)
-            {
-                pushSwitch(ref isPlaying);
-                Console.WriteLine(isPlaying);
-                if (isPlaying)
+                if (cofd.ShowDialog() != CommonFileDialogResult.Ok)
                 {
-                    MediaElementPlayView.Play();
+                    return;
                 }
-                else
-                {
-                    MediaElementPlayView.Pause();
-                }
-            }
 
-            Console.WriteLine(e.Key);
-        }
-
-        private void Window_PadUpdate(JoystickState jState)
-        {
-            // 挙動確認用：押されたキーをタイトルバーに表示する
-            // アナログスティックの左右軸
-            bool inputX = true;
-            if (jState.X > 300)
-            {
-                Console.WriteLine("入力キー：→");
+                // FileNameで選択されたフォルダを取得する
+                FolderTextbox.Text = cofd.FileName;
             }
-            else if (jState.X < -300)
-            {
-                Console.WriteLine("入力キー：←");
-            }
-            else
-            {
-                inputX = false;
-            }
-            // アナログスティックの上下軸
-            bool inputY = true;
-            if (jState.Y > 300)
-            {
-                Console.WriteLine("入力キー：↓");
-            }
-            else if (jState.Y < -300)
-            {
-                Console.WriteLine("入力キー：↑");
-            }
-            else
-            {
-                inputY = false;
-            }
-            // 未入力だった場合
-            if (!inputX && !inputY)
-            {
-                //Text = "入力キー：";
-            }
-
-            // ボタン判定
-            if (jState.Buttons[0])
-            {
-                // 1番ボタンが押されたらフォームが閉じるように処理
-                //Close();
-                Console.WriteLine("Buttons[0]");
-               
-            }
-        }
-
-        private void Window_Loaded(object sender, RoutedEventArgs e)
-        {
-            user_inp = new UserInput(true);
-            user_inp.SetPadUpdateFunc(Window_PadUpdate);
-            user_inp.StartThread();
-        }
-
-        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            // PadThreadFuncメソッドを終了
-            user_inp.StopThread();
         }
     }
 }
--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# OXIDEfactoryとは
-ゲームのコントローラーで操作できる動画編集ソフトを目指しています。
+# batchLauncherTemplateとは
+Batchファイル実行アプリのテンプレートです。
 
 # 特徴
-* コントローラー入力による素早い操作
-* プレビューの高速性
+* フォルダ？
 
 # 開発環境
 * Windows10
 * VisualStudio2019
-* C# WPF .NET Core 3.1
+* C# WPF .NET 5.0
